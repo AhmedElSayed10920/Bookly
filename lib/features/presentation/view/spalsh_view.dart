33e1ddc8ff2260fef1a59a9b223ce322f721a4e3
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-06-17 01:20:11 +0300

    created splash view

---
+++ lib/features/presentation/view/spalsh_view.dart
@@ -0,0 +1,13 @@
+import 'package:booklyapp/features/presentation/view/widgets/spalsh_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SpalshView extends StatelessWidget {
+  const SpalshView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:booklyapp/constants.dart';
+import 'package:booklyapp/features/presentation/view/spalsh_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          scaffoldBackgroundColor: Color(kPrimaryColor.value),
+          brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      home: const SpalshView(),
+    );
+  }
+}
+
+
