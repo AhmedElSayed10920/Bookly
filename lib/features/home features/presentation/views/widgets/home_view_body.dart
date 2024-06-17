0545f1882416f86a0f03196f80f8af1b8ee8bb67
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-06-18 02:50:55 +0300

    added styles file to my app

--- lib/features/home features/presentation/views/widgets/home_view_body.dart
+++ lib/features/home features/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:booklyapp/core/utils/styles.dart';
 import 'package:booklyapp/features/home%20features/presentation/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/features/home%20features/presentation/views/widgets/featured_book_list.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +11,18 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SizedBox(
           height: 40,
         ),
         CustomAppBar(),
         FeaturedBookList(),
+        SizedBox(height: 40,),
+        Padding(
+          padding: EdgeInsets.only(left: 16),
+          child: Text('Best Seller',style: Styles.mediumTitle,),
+        )
       ],
     );
   }
