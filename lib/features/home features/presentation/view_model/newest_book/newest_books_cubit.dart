28c30e45d90b88885629d6dabcc0ca02d12d2bbd
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-06-21 06:23:24 +0300

    newest book cubit done

---
+++ lib/features/home features/presentation/view_model/newest_book/newest_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:booklyapp/features/home%20features/data/models/books_model/books_model.dart';
+import 'package:booklyapp/features/home%20features/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
