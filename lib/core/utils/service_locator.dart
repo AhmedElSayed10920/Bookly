c7acdde056ba00855490c47f96637096744654c9
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-06-24 01:11:37 +0300

    added service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:booklyapp/core/utils/api_service.dart';
+import 'package:booklyapp/features/home%20features/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      getIt.get<ApiService>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:booklyapp/constants.dart';
 import 'package:booklyapp/core/utils/app_router.dart';
+import 'package:booklyapp/core/utils/service_locator.dart';
+import 'package:booklyapp/features/home%20features/data/repos/home_repo_impl.dart';
+import 'package:booklyapp/features/home%20features/presentation/view_model/featured_book/featured_book_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const Bookly());
 }
 
@@ -12,14 +17,22 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color(kPrimaryColor.value),
-        brightness: Brightness.dark,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) =>
+                FeaturedBookCubit(getIt.get<HomeRepoImpl>()))
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Color(kPrimaryColor.value),
+          brightness: Brightness.dark,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
