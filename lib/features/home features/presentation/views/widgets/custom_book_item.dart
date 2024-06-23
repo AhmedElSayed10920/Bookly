ba40654564afd0cc486162e7100069d4a6a557a5
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-06-24 02:37:54 +0300

    added cached network image refactor

--- lib/features/home features/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home features/presentation/views/widgets/custom_book_item.dart
@@ -1,3 +1,5 @@
+import 'package:booklyapp/core/widgets/custom_progress_indicator.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -9,14 +11,16 @@ class CustomBookImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.amber,
-              image: DecorationImage(
-                  image: NetworkImage(imageUrl), fit: BoxFit.fill))),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+          aspectRatio: 2.6 / 4,
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            placeholder: (context, url) => const CustomProgressIndicator() ,
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            fit: BoxFit.fill,
+          )),
     );
   }
 }
